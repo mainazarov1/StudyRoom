d91426557bdc9edc01d97e66fa61d01d085f770a
mainazarov1 <mainazarov1@gmail.com>
2022-11-09 19:05:09 +0600

    feat: add inputs components for auth

---
+++ src/components/InputApp/InputApp.tsx
@@ -0,0 +1,15 @@
+import { Form, Input } from 'antd';
+
+import styles from './InputApp.module.scss';
+interface IInputAppProps {
+  label: string;
+  name: string;
+  placeholder: string;
+}
+export const InputApp = ({ label, name, placeholder }: IInputAppProps) => {
+  return (
+    <Form.Item className={styles.inputAppLabel} label={label} name={name}>
+      <Input className={styles.inputApp} placeholder={placeholder} />
+    </Form.Item>
+  );
+};
