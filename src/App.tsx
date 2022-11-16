e45c2143d44b5b44b868c452533ff49d50162fe6
Eldiyar888 <jashtilekov888@gmail.com>
2022-11-16 16:57:23 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import './App.css';
+import Header from './components/Header/Header';
+import 'antd/dist/antd.css';
+import { HeaderLogoPlus } from './components/Header/HeaderLogoPlus/HeaderLogoPlus';
+import { HeaderTest } from './components/Header/HeaderLogoPlus/HeaderTest';
+import RightComponent from './components/Header/RightCoponent/RightComponent';
+
+let middleItems = [
+  {
+    title: 'Лента',
+    path: '/appointed'
+  },
+  {
+    title: 'Задания',
+    path: '/term'
+  },
+  {
+    title: 'Пользователи',
+    path: '/performed'
+  }
+]
+
+let leftTrigger = {
+  title: 'dwaaw',
+  path: '/logo'
+}
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Header children={<HeaderLogoPlus leftTrigger={leftTrigger} middleItems={middleItems}  rightComponent={<RightComponent />}/>} />
+    </div>
+  )
 }
 
 export default App;
