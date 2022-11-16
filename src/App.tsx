13735d62420d6b642dcd3a2bba114d60d175af31
mainazarov1 <mainazarov1@gmail.com>
2022-11-16 16:50:43 +0600

    feat: add routes and paths; fix: eslint configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import React from 'react'
-import Home from './pages/Home'
-import { CourseCard } from './containers/CourseCard/CourseCard';
-import { Tape } from './containers/Tape/Tape';
-import { Auth } from './pages/Auth/Auth';
-import { DragDropContext } from "react-beautiful-dnd";
-import { useState } from 'react';
 import './App.css';
-import Tasks from './pages/Tasks/Tasks';
+import { MainRoutes } from './routes/routes';
 
 function App() {
   return (
     <div className='App'>
-			{/* <Auth /> */}
-			<Tape />
-      {/* <Tasks /> */}
-      <Home />
-      {/* <CourseCard /> */}
-    </div>)
+      <MainRoutes />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,83 @@
+import React, { useId } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { Home } from '../pages/Home/Home';
+import { Auth } from '../pages/Auth/Auth';
+import { LayoutApp } from '../containers/LayoutApp/LayoutApp';
+import {
+  AUTH_PAGE,
+  MAIN_PAGE,
+  // TAPE_PAGE,
+  // TASKS_PAGE,
+  // USERS_PAGE,
+  // RATING_PAGE
+} from '../utils/path/path';
+
+interface RouterData {
+  id?: string;
+  path: string;
+  component: JSX.Element;
+}
+
+const MainRoutes = () => {
+  const PUBLIC_ROUTES: RouterData[] = [
+    // {
+    //   id: useId(),
+    //   path: AUTH_PAGE,
+    //   component: <Auth />,
+    // },
+    {
+      id: useId(),
+      path: MAIN_PAGE,
+      component: <Home />,
+    },
+    // {
+    // 	id: useId(),
+    // 	path: TAPE_PAGE,
+    // 	component: <Tape />
+    // },
+    // {
+    // 	id: useId(),
+    // 	path: TASKS_PAGE,
+    // 	component: <Tasks />
+    // },
+    // {
+    // 	id: useId(),
+    // 	path: USERS_PAGE,
+    // 	component: <Users />
+    // },
+    // {
+    // 	id: useId(),
+    // 	path: RATING_PAGE,
+    // 	component: <Rating />
+    // },
+  ];
+
+  // const PRIVATE_ROUTES: RouterData[] = [
+  //   {
+  //     id: useId(),
+  //     link: ADMIN_MAIN_PAGE,
+  //     element: <AdminPage />,
+  //   },
+  // ];
+
+  return (
+    <React.Suspense fallback={<span> Loading...</span>}>
+      <Routes>
+        <Route path={AUTH_PAGE} element={<Auth />} />
+        <Route element={<LayoutApp />}>
+          {PUBLIC_ROUTES.map(({ id, path, component }) => (
+            <Route path={path} element={component} key={id} />
+          ))}
+        </Route>
+        {/* <Route>
+					{PRIVATE_ROUTES.map(({ id, link, element }) => (
+						<Route path={link} element={element} key={id} />
+					))}
+				</Route> */}
+      </Routes>
+    </React.Suspense>
+  );
+};
+
+export { MainRoutes };
