c36c757a5888ec3aaf54ba3ea6ff55450e7cff30
mainazarov1 <mainazarov1@gmail.com>
2022-12-06 12:46:57 +0600

    feat: add Button to Users component

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,27 +1,29 @@
+import { MoreOutlined, UserAddOutlined } from "@ant-design/icons"
 import { Divider, Layout, Typography } from "antd"
 import { Content } from "antd/lib/layout/layout"
+import { ButtonApp } from "../../components/ButtonApp/ButtonApp"
 import styles from './Users.module.scss'
 const { Title } = Typography
 const users = [
 	{
 		fullname: 'Иванов Иван Иванович',
 		role: 'admin',
-		avatar: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+		avatar: 'https://www.nea.org/sites/default/files/legacy/2020/04/new_teacher.jpeg',
 	},
 	{
 		fullname: 'Иванов',
 		role: 'teacher',
-		avatar: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+		avatar: 'https://www.nea.org/sites/default/files/legacy/2020/04/new_teacher.jpeg',
 	},
 	{
 		fullname: 'Иванович',
 		role: 'student',
-		avatar: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+		avatar: 'https://www.nea.org/sites/default/files/legacy/2020/04/new_teacher.jpeg',
 	},
 	{
 		fullname: 'Иванов Иван Иванович',
 		role: 'student',
-		avatar: 'https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg',
+		avatar: 'https://www.nea.org/sites/default/files/legacy/2020/04/new_teacher.jpeg',
 	},
 ]
 export const Users = () => {
@@ -32,19 +34,28 @@ export const Users = () => {
 			<div className={styles.users__wrap}>
 				<div className={styles.users__column}>
 					<header>
-						<Title className={styles.users__title} level={5}>Users</Title>
-						<button>+</button>
+						<Title className={styles.users__title} level={5}>Преподаватели</Title>
+						<ButtonApp icon={<UserAddOutlined />} />
 					</header>
 					<Divider />
 					<div className={styles.users__list}>
 						{
-							users.map((user, index) => {
+							users.filter(user => user.role === 'teacher' || user.role === 'admin').map((user, index) => {
 								return (
 									<div className={styles.users__item} key={index}>
 										<img src={user.avatar} alt="avatar" />
 										<div className={styles.users__info}>
 											<div className={styles.users__name}>{user.fullname}</div>
-											<div className={styles.users__role}>{user.role}</div>
+											{user.role === 'teacher' &&
+												<ButtonApp
+													// style={{
+													// 	background: 'transparent',
+													// 	'&:hover': {
+													// 		backgroundColor: 'red',
+													// 	},
+													// }}
+													icon={<MoreOutlined />} />
+											}
 										</div>
 									</div>
 								)
