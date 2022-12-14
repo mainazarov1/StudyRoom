995a323884e34a5555369efd44c9ea2b0afcff6c
beku026 <beksultanbakybekov@gmail.com>
2022-12-14 11:33:33 +0600

    error corection

--- src/containers/SideBar/MenuAside.tsx
+++ src/containers/SideBar/MenuAside.tsx
@@ -53,7 +53,7 @@ const MenuAside:FC<IProps> = ( { listStudenCurs, ListTeacherCurs, onClose }) => 
       </Menu.Item>
       {
         listStudenCurs?.map((item) => (
-          <SideBarMenuItem key={item.id} item={item} />
+          <SideBarMenuItem key={item.id} onClose={onClose} item={item} />
         ))
       }
       <Divider/>
