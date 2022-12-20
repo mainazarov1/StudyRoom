87627b1e508768cc154261ca1b3ee71b0e5856e0
mainazarov1 <mainazarov1@gmail.com>
2022-12-20 19:14:55 +0600

    refactor: refactor code with lint

--- src/components/UsersTable/UsersTable.tsx
+++ src/components/UsersTable/UsersTable.tsx
@@ -1,36 +1,40 @@
-import { DownOutlined, MoreOutlined } from "@ant-design/icons"
-import { Avatar, Dropdown, Space, Table } from "antd"
-import { type ColumnsType } from "antd/es/table"
-import { TableRowSelection } from "antd/lib/table/interface"
-import styles from './../../pages/Users/Users.module.scss'
+import { DownOutlined, MoreOutlined } from '@ant-design/icons';
+import { Avatar, Dropdown, Space, Table } from 'antd';
+import { type ColumnsType } from 'antd/es/table';
+import { TableRowSelection } from 'antd/lib/table/interface';
+
+import styles from './../../pages/Users/Users.module.scss';
 // interface DataType {
 // 	key: number,
 // 	fullname: string,
 // 	avatar: JSX.Element,
 // }
-interface UsersTableProps { 
-	columns: ColumnsType<unknown>,
-	data: any[],
-	checkbox: boolean,
-	showHeader: boolean
+interface UsersTableProps {
+  columns: ColumnsType<unknown>;
+  data: any[];
+  checkbox: boolean;
+  showHeader: boolean;
 }
-export const UsersTable = ({columns, data, checkbox, showHeader = false, ...args}: UsersTableProps) => {
-	
-	
-	return (
-		<Table
-			// size="small"
-			pagination={false}
-			showHeader={showHeader}
-			columns={columns}
-			dataSource={data}
-			{...args}
-			rowSelection={
-				(checkbox &&
-				{
-				columnWidth:'10px'
-				})
-			}
-		/>
-	)
-}
+export const UsersTable = ({
+  columns,
+  data,
+  checkbox,
+  showHeader = false,
+  ...args
+}: UsersTableProps) => {
+  return (
+    <Table
+      // size="small"
+      pagination={false}
+      showHeader={showHeader}
+      columns={columns}
+      dataSource={data}
+      {...args}
+      rowSelection={
+        checkbox && {
+          columnWidth: '10px',
+        }
+      }
+    />
+  );
+};
--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -1,56 +1,58 @@
 import { MoreOutlined, UserAddOutlined } from '@ant-design/icons';
 import { Divider, Table, Tooltip, Typography } from 'antd';
+
 import { UsersTable } from '../../components/UsersTable/UsersTable';
-import { studentsColumns, studentsData, teachersColumns, teachersData } from '../../utils/mock/tableMockApi';
+import {
+  studentsColumns,
+  studentsData,
+  teachersColumns,
+  teachersData,
+} from '../../utils/mock/tableMockApi';
 
 import styles from './Users.module.scss';
 const { Title } = Typography;
 
 export const Users = () => {
-	return (
-		<div className={styles.users}>
-			<div className={styles.users__wrap}>
-				<div className={styles.users__column}>
-					<header>
-						<Title className={styles.users__title} level={5}>
-							Преподаватели
-						</Title>
-						<div className={styles.tooltip}>
-							<Tooltip title='Пригласить преподавателей' placement='bottom'>
-								<UserAddOutlined
-									className={styles.users__icon}
-									onClick={() => alert('click')}
-								/>
-							</Tooltip>
-
-						</div>
-					</header>
-					<Divider
-						className={styles.users__divider}
-					/>
-					<UsersTable columns={teachersColumns} data={teachersData} />
-				</div>
-				<div className={styles.users__column}>
-					<header>
-						<Title className={styles.users__title} level={5}>
-							Учащиеся
-						</Title>
-						<Typography.Paragraph className={styles.users__text}>{`${studentsData.length} учащихся`}</Typography.Paragraph>
-						<div className={styles.users__tooltip}>
-							<Tooltip title='Пригласить учащихся' placement='bottom'>
-								<UserAddOutlined
-									className={styles.users__icon}
-									onClick={() => alert('click')}
-								/>
-							</Tooltip>
-						</div>
-					</header>
-					<Divider
-						className={styles.users__divider}
-					/>
-					<UsersTable columns={studentsColumns} data={studentsData} showHeader={true} checkbox={true} />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.users}>
+      <div className={styles.users__wrap}>
+        <div className={styles.users__column}>
+          <header>
+            <Title className={styles.users__title} level={5}>
+              Преподаватели
+            </Title>
+            <div className={styles.tooltip}>
+              <Tooltip title='Пригласить преподавателей' placement='bottom'>
+                <UserAddOutlined className={styles.users__icon} onClick={() => alert('click')} />
+              </Tooltip>
+            </div>
+          </header>
+          <Divider className={styles.users__divider} />
+          <UsersTable columns={teachersColumns} data={teachersData} />
+        </div>
+        <div className={styles.users__column}>
+          <header>
+            <Title className={styles.users__title} level={5}>
+              Учащиеся
+            </Title>
+            <Typography.Paragraph
+              className={styles.users__text}
+            >{`${studentsData.length} учащихся`}</Typography.Paragraph>
+            <div className={styles.users__tooltip}>
+              <Tooltip title='Пригласить учащихся' placement='bottom'>
+                <UserAddOutlined className={styles.users__icon} onClick={() => alert('click')} />
+              </Tooltip>
+            </div>
+          </header>
+          <Divider className={styles.users__divider} />
+          <UsersTable
+            columns={studentsColumns}
+            data={studentsData}
+            showHeader={true}
+            checkbox={true}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
